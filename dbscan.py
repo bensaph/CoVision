ff39c3b1a0a63e9155350fd209ccef7f48201331
sw478 <silas.wong478@gmail.com>
2020-10-30 16:20:05 -0700

    added cluster lists

--- dbscan.py
+++ dbscan.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
 from tkinter import *
-from enum import Enum
+
+# https://en.wikipedia.org/wiki/DBSCAN
 
 # adjustable constants
 
@@ -15,28 +16,29 @@ point_radius = dim * 0.05
 pts_size = 50 # number of entities
 minPts = 3 # number of neighbors within epsilon to be considered a core point
 epsilon = point_radius * 2 # within epsilon if two circles overlap
+num_clusters = 1
 
 # for movement (after one timestep)
-lin_speed_min = dim * 0.005 # linear speed
-lin_speed_range = dim * 0.01 # randomized lin speed between min and min+range
-ang_speed_range = (np.pi * 2) * 0.25 # range of possible rotation
+lin_speed_min = dim * 0.01 # linear speed minimum
+lin_speed_range = dim * 0.00 # randomized lin speed between min and min+range
+ang_speed_range = (np.pi * 2) * 0.2 # range of possible rotation
 
-# https://en.wikipedia.org/wiki/DBSCAN
-class Label(Enum):
-    unlabeled = 0 # not processed
-    core = 1
-    reachable = 2 # "border points"
-    noise = 3 # outliers (not part of cluster)
+colors = ["green", "black", "red", "orange"]
+unlabeled = 0 # not processed
+core = 1 # (part of cluster)
+reachable = 2 # "border points" (part of cluster)
+noise = 3 # outliers (not part of cluster)
 
 class Point:
-    def __init__(self, x=0.0, y=0.0, dir=0.0, master=None, canvas=None):
+    def __init__(self, id, x=0.0, y=0.0, dir=0.0, master=None, canvas=None):
         self.master = master
         self.canvas = canvas
         self.canvas.pack()
-        self.x = random.random() * (x_dim - 2 * point_radius) + point_radius
-        self.y = random.random() * (y_dim - 2 * point_radius) + point_radius
+        self.id = id
+        self.x = random.random() * x_dim # - 2 * point_radius) + point_radius
+        self.y = random.random() * y_dim # - 2 * point_radius) + point_radius
         self.dir = random.random() * (np.pi * 2)
-        self.label = Label.unlabeled
+        self.label = unlabeled
         self.cluster = -1 # indicates noise
 
         self.point = self.canvas.create_oval(
@@ -44,7 +46,7 @@ class Point:
             self.y - point_radius,
             self.x + point_radius,
             self.y + point_radius,
-            outline = get_color(self.label)
+            outline = colors[self.label]
         )
 
     def movement(self):
@@ -52,6 +54,7 @@ class Point:
         angle = (random.random() * ang_speed_range * 2) - ang_speed_range
         dx = speed * np.cos(self.dir)
         dy = speed * np.sin(self.dir)
+
         self.x += dx
         self.y += dy
         self.dir += angle
@@ -66,7 +69,7 @@ def main():
     canvas = Canvas(master, width=x_dim, height=y_dim)
 
     # initialize starting positions
-    pts = [Point(master=master, canvas=canvas) for i in range(pts_size)]
+    pts = [Point(i, master=master, canvas=canvas) for i in range(pts_size)]
 
     moveAll(pts)
     master.bind("<space>", lambda e: moveAll(pts)) # press space to step through
@@ -79,27 +82,46 @@ def moveAll(pts):
 
     # recalculate clusters
     for p in pts:
-        p.label = Label.unlabeled
-        p.cluster = -1
+        p.label = unlabeled
+        #p.cluster = -1
     dbscan(pts, minPts, epsilon)
 
     # recolor
     for p in pts:
-        p.canvas.itemconfig(p.point, outline=get_color(p.label))
+        p.canvas.itemconfig(p.point, outline=colors[p.label])
+
+    printStats(pts)
+
+def printStats(pts):
+    # arbitrary length right now
+    clusters = [[] for i in range(num_clusters)]
+    for p in pts:
+        if p.cluster == -1:
+            continue
+        clusters[p.cluster].append(p)
+
+    for i in range(len(clusters)):
+        print("cluster {}:".format(i), end="")
+        for p in clusters[i]:
+            print('\t{}'.format(p.id), end="")
+        print()
+    print()
+
 
 # called every timestep
 def dbscan(pts, minPts=3, epsilon=point_radius*2):
+    global num_clusters
     c = 0 # cluster counter
     for p in pts:
-        if p.label is not Label.unlabeled:
+        if p.label is not unlabeled:
             continue
         n = within_range(pts, p, epsilon)
         if len(n) < minPts:
-            p.label = Label.noise # initialize as noise, can change later
+            p.label = noise # initialize as noise, can change later
             continue
 
         p.cluster = c
-        p.label = Label.core
+        p.label = core
 
         # classifies a core node's neighbors as either
         # another core node and adds its neighbors to the set
@@ -107,20 +129,22 @@ def dbscan(pts, minPts=3, epsilon=point_radius*2):
         n = n.difference({p})
         while len(n) > 0:
             q = n.pop()
-            if q.label is Label.noise:
-                q.label = Label.reachable
+            if q.label is noise:
+                q.label = reachable
                 q.cluster = c
-            if q.label is not Label.unlabeled:
+            if q.label is not unlabeled:
                 continue
 
             q.cluster = c
-            q.label = Label.reachable
+            q.label = reachable
 
             nq = within_range(pts, q, epsilon)
             if len(nq) >= minPts:
-                q.label = Label.core
+                q.label = core
                 n = n.union(nq)
         c += 1
+    if(c > num_clusters):
+        num_clusters = c
 
 # returns set of Points within range of epsilon including itself
 def within_range(pts, p_org, epsilon):
@@ -129,17 +153,6 @@ def within_range(pts, p_org, epsilon):
         if p_org.distance(p) <= epsilon:
             n.add(p)
     return n
-
-# probably a better way to do this but not important
-def get_color(label):
-    if label is Label.noise:
-        return "orange"
-    if label is Label.reachable:
-        return "red"
-    if label is Label.core:
-        return "black"
-    if label is Label.unlabeled: # shouldn't be any
-        return "green"
 
 if __name__ == "__main__":
     main()
