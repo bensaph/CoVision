9b4741d4a659b8d764cf8fe8ba3eb3a339407e98
sw478 <silas.wong478@gmail.com>
2020-10-30 13:48:10 -0700

    dbscan: space to step through time

--- dbscan.py
+++ dbscan.py
@@ -3,6 +3,24 @@ import random
 from tkinter import *
 from enum import Enum
 
+# adjustable constants
+
+# for drawing
+dim = 900
+x_dim = dim * 1
+y_dim = dim * 1
+point_radius = dim * 0.05
+
+# for dbscan
+pts_size = 50 # number of entities
+minPts = 3 # number of neighbors within epsilon to be considered a core point
+epsilon = point_radius * 2 # within epsilon if two circles overlap
+
+# for movement (after one timestep)
+lin_speed_min = dim * 0.005 # linear speed
+lin_speed_range = dim * 0.01 # randomized lin speed between min and min+range
+ang_speed_range = (np.pi * 2) * 0.25 # range of possible rotation
+
 # https://en.wikipedia.org/wiki/DBSCAN
 class Label(Enum):
     unlabeled = 0 # not processed
@@ -11,54 +29,65 @@ class Label(Enum):
     noise = 3 # outliers (not part of cluster)
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x=0.0, y=0.0, dir=0.0, master=None, canvas=None):
+        self.master = master
+        self.canvas = canvas
+        self.canvas.pack()
+        self.x = random.random() * (x_dim - 2 * point_radius) + point_radius
+        self.y = random.random() * (y_dim - 2 * point_radius) + point_radius
+        self.dir = random.random() * (np.pi * 2)
         self.label = Label.unlabeled
         self.cluster = -1 # indicates noise
+
+        self.point = self.canvas.create_oval(
+            self.x - point_radius,
+            self.y - point_radius,
+            self.x + point_radius,
+            self.y + point_radius,
+            outline = get_color(self.label)
+        )
+
+    def movement(self):
+        speed = (random.random() * lin_speed_range) + lin_speed_min
+        angle = (random.random() * ang_speed_range * 2) - ang_speed_range
+        dx = speed * np.cos(self.dir)
+        dy = speed * np.sin(self.dir)
+        self.x += dx
+        self.y += dy
+        self.dir += angle
+        self.canvas.move(self.point, dx, dy)
 
     # distance formula
     def distance(self, other):
         return np.sqrt(np.power(self.x - other.x, 2) + np.power(self.y - other.y, 2))
 
-# adjustable constants
-dim = 500
-x_dim = dim * 1
-y_dim = dim * 1
-point_radius = dim * 0.05
-
-pts_size = 50 # number of entities
-minPts = 3 # number of neighbors within epsilon to be considered a core point
-epsilon = point_radius * 2 # within epsilon if two circles overlap
-
 def main():
-    # initialize starting positions
-    pts = [Point(
-        random.random() * (x_dim - 2 * point_radius) + point_radius,
-        random.random() * (y_dim - 2 * point_radius) + point_radius)
-        for i in range(pts_size)]
+    master = Tk()
+    canvas = Canvas(master, width=x_dim, height=y_dim)
 
+    # initialize starting positions
+    pts = [Point(master=master, canvas=canvas) for i in range(pts_size)]
+
+    moveAll(pts)
+    master.bind("<space>", lambda e: moveAll(pts)) # press space to step through
+    canvas.mainloop()
+
+def moveAll(pts):
+    # move to next position
+    for p in pts:
+        p.movement()
+
+    # recalculate clusters
+    for p in pts:
+        p.label = Label.unlabeled
+        p.cluster = -1
     dbscan(pts, minPts, epsilon)
 
-    master = Tk()
-    w = Canvas(master, width=x_dim, height=y_dim)
-    w.pack()
-
-    clusters = []
+    # recolor
     for p in pts:
-        w.create_oval(
-            p.x - point_radius,
-            p.y - point_radius,
-            p.x + point_radius,
-            p.y + point_radius,
-            outline = get_color(p.label)
-        )
-        print('{}\t{} {}'.format(p.label, "cluster:", p.cluster))
-    #print('{}{}'.format("minPts:", minPts))
-    #print('{}{}'.format("epsilon:", epsilon))
+        p.canvas.itemconfig(p.point, outline=get_color(p.label))
 
-    w.mainloop()
-
+# called every timestep
 def dbscan(pts, minPts=3, epsilon=point_radius*2):
     c = 0 # cluster counter
     for p in pts:
